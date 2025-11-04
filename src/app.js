4b2085041e8ac606eb7ca60c80bc2eeca4b465f9
yusuf <horiyorrmi72@gmail.com>
2025-11-04 06:57:51 +0100

    feat(start server):add a start server function to handle server startup only when database is connected

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ import { connectDB } from "./config/db.js";
 import studentRoutes from "./routes/studentRoutes.js";
 import courseRoutes from "./routes/courseRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
+import logger from "./utils/logger.js";
 
 dotenv.config();
-connectDB();
 
 const app = express();
 app.use(express.json());
@@ -25,4 +25,20 @@ app.use(errorHandler);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
+
+const startServer = async () => {
+  try
+  {
+    await connectDB();
+    app.listen(PORT, () => logger.info(`✅ Server running on port ${PORT}`));
+
+
+  } catch (err)
+  {
+    logger.error('error starting server', err.message);
+    process.exit(1);
+
+  }
+}
+startServer()
